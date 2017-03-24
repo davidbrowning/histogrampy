320bf9887eb21b3ef2e9e64d6ec75f97f2f0f26e
david browning <dmbrowning21@gmail.com>
2017-03-24 13:11:38 -0600

    added test cases

---
+++ test.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+python hist_image_index.py -imgdir images/ -clr rgb -hist rgb_hist16.pck -bin 16
+python hist_image_index.py -imgdir images/ -clr hsv -hist hsv_hist16.pck -bin 16
+
+
+exit
