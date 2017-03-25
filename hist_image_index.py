90d07f66238e5fdba45cfaafd9e330393acaef6c
david browning <dmbrowning21@gmail.com>
2017-03-25 15:53:03 -0600

    indexing works, (I think)

--- hist_image_index.py
+++ hist_image_index.py
@@ -39,14 +39,24 @@ args = vars(ap.parse_args())
 
 HIST_INDEX = {}
 
+def generate_file_names(fnpat, rootdir):
+    for path,dirlist, filelist in os.walk(rootdir):
+        for file_name in filelist:
+            if(re.match(fnpat, file_name)):
+                yield (os.path.join(path, file_name))
+
 def hist_index_img(imgp, color_space, bin_size=8):
+  im = cv2.imread(imgp)
   global HIST_INDEX
-  ## your code
-  pass
+  if(color_space == 'rgb'):
+    HIST_INDEX[imgp] = cv2.calcHist([im], [0,1,2], None, [bin_size,bin_size,bin_size], [0,256,0,256,0,256])
+  elif(color_space == 'hsv'):
+    hsv_im = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
+    HIST_INDEX[imgp] = cv2.calcHist([hsv_im], [0,1,2], None, [bin_size,bin_size,bin_size], [0,256,0,256,0,256])
 
 def hist_index_img_dir(imgdir, color_space, bin_size):
-  # your code
-  pass
+    for img in generate_file_names(r'.+\.(jpg|png|JPG)', imgdir):
+        hist_index_img(img, color_space, bin_size)
 
 if __name__ == '__main__':
   hist_index_img_dir(args['imgdir'], args['clr'], int(args['bin']))
--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
-#!/usr/bin/python
+#!/bin/bash
 
 python hist_image_index.py -imgdir images/ -clr rgb -hist rgb_hist16.pck -bin 16
+
 python hist_image_index.py -imgdir images/ -clr hsv -hist hsv_hist16.pck -bin 16
 
 
